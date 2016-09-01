82e4bba1ea60b7d5eacaabf8d60bee1a8a74a44d
Lesiak <KrzyLech@gmail.com>
2016-09-01 10:31:06 +0200

    kodzik

--- PKW/static/scripts.js
+++ PKW/static/scripts.js
@@ -339,3 +339,42 @@ $('#inh_button').click(function(){
         success: produce_table
     });
 })
+
+//Obsługa zapisywania wprowadzonych zmian do bazy danych
+$(".modal").on("click", ".save_button", function() {
+    var votes_for_first = $(this).parent().siblings().children(".vote_input1").val();
+    var votes_for_second = $(this).parent().siblings().children(".vote_input2").val();
+    var dis_name = $(this).parent().siblings(".dis_name").text();
+    $.ajax({
+        type:'GET',
+        url:'http://127.0.0.1:8000/viewing/save',
+        data: {
+            dn: dis_name
+        },
+        success: function(data) {
+            question = "Ostatnia edycja miała miejsce " +
+                data +
+                ". Czy na pewno chcesz dokonać zmiany?";
+            if(confirm(question)) {
+                $.ajax({
+                    type:'POST',
+                    url:'http://127.0.0.1:8000/viewing/save',
+                    data: {
+                        v1: votes_for_first,
+                        v2: votes_for_second,
+                        dn: dis_name,
+                    },
+                    success: function() {
+                        alert("Changes have been saved");
+                    },
+					error: function() {
+						alert("Error saving changes");
+					}
+                })
+            }
+        },
+		error: function(data) {
+			alert("Error getting date of last edit");
+		}
+    });
+});
--- PKW/views.py
+++ PKW/views.py
@@ -51,7 +51,7 @@ def editing_byinh(request):
 	lb = request.GET['lower']
 	ub = request.GET['upper']
 	data = serializers.serialize('json',District.objects.filter(Inhabitants__range=[lb,ub]))
-	response = HttpResponse(data);
+	response = HttpResponse(data)
 	response["Access-Control-Allow-Origin"] = "*"
 	return response;
 
@@ -60,7 +60,7 @@ def editing_byinh(request):
 def editing_byvoiv(request):
 	vname = request.GET['name']
 	data = serializers.serialize('json', District.objects.filter(voivodeship__Name=vname))
-	response = HttpResponse(data);
+	response = HttpResponse(data)
 	response["Access-Control-Allow-Origin"] = "*"
 	return response;
 
@@ -69,27 +69,27 @@ def editing_byvoiv(request):
 def editing_bytype(request):
 	tName = request.GET['name']
 	data = serializers.serialize('json', District.objects.filter(Type=tName))
-	response = HttpResponse(data);
+	response = HttpResponse(data)
 	response["Access-Control-Allow-Origin"] = "*"
 	return response;
 
 
 def editing_save(request):
-    if request.method == 'GET':
-        dn = request.GET['dn']
-        last_edit = District.objects.filter(Name=dn).values('last_edit_date')
+	if request.method == 'GET':
+		dn = request.GET['dn']
+		last_edit = District.objects.filter(Name=dn).values('last_edit_date')
 		response = HttpResponse(last_edit[0]['last_edit_date'])
 		response["Access-Control-Allow-Origin"] = "*"
-        return response
-    if request.method == 'POST':
-        v1 = request.POST['v1']
-        v2 = request.POST['v2']
-        rel_v = int(v1) + int(v2)
-        dn = request.POST['dn']
-        dis = District.objects.get(Name=dn)
-        dis.votes_for_first = v1
-        dis.relevant_votes = rel_v
-        dis.save()
+		return response
+	if request.method == 'POST':
+		v1 = request.POST['v1']
+		v2 = request.POST['v2']
+		rel_v = int(v1) + int(v2)
+		dn = request.POST['dn']
+		dis = District.objects.get(Name=dn)
+		dis.votes_for_first = v1
+		dis.relevant_votes = rel_v
+		dis.save()
 		response = HttpResponse();
 		response["Access-Control-Allow-Origin"] = "*"
-        return HttpResponse();
+		return HttpResponse();
